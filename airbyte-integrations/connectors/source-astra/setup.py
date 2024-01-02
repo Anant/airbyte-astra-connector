371823a702d803ef29f3503d9930d29fd3c3613e
Obioma Anomnachi <onanomnachi@gmail.com>
2024-01-02 19:38:19 +0000

    Unit test fixes

--- airbyte-integrations/connectors/source-astra/setup.py
+++ airbyte-integrations/connectors/source-astra/setup.py
@@ -14,8 +14,7 @@ MAIN_REQUIREMENTS = [
 TEST_REQUIREMENTS = [
     "requests-mock~=1.9.3",
     "pytest-mock~=3.6.1",
-    "pytest~=6.2",
-    "connector-acceptance-test",
+    "pytest~=6.2"
 ]
 
 setup(
