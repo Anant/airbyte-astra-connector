786b41b9bd5b4b98d8328c55e23c0230a19c7157
ElenaKusevska <elena.kusevska@gmail.com>
2024-01-08 02:00:38 +0100

    add integration tests for check with valid and invalid config

--- airbyte-integrations/connectors/destination-astra/integration_tests/integration_test.py
+++ airbyte-integrations/connectors/destination-astra/integration_tests/integration_test.py
@@ -2,7 +2,26 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
+import logging
 
-def integration_test():
-    # TODO write integration tests
-    pass
+from airbyte_cdk.destinations.vector_db_based.test_utils import BaseIntegrationTest
+from airbyte_cdk.models import DestinationSyncMode, Status
+from destination_astra.destination import DestinationAstra
+
+
+class AstraIntegrationTest(BaseIntegrationTest):
+
+    def test_check_valid_config(self):
+        outcome = DestinationAstra().check(logging.getLogger("airbyte"), self.config)
+        assert outcome.status == Status.SUCCEEDED
+
+    def test_check_invalid_config(self):
+        invalid_config = self.config 
+
+        invalid_config["embedding"]["openai_key"] = 123
+
+        outcome = DestinationAstra().check(
+            logging.getLogger("airbyte"), invalid_config)
+        assert outcome.status == Status.FAILED
+
+
